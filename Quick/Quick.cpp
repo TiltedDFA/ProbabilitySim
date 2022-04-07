9e1bf4d79b5bb5e90b68fa881253cd3c1de8d92f
TiltedDFA <p5mtremain@rmgs.org.uk>
2022-04-07 18:31:35 +0100

    a few improvements with use, still need to get the percentage to display properly

--- Quick/Quick.cpp
+++ Quick/Quick.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <algorithm>
-#include <array>
+#include <ctime>
 #include <chrono>
 
+constexpr int _t = 100000000;
 
 int main()
 {     
-    auto start = std::chrono::high_resolution_clock::now();
-    std::vector<int>Deck;    
-    float ap=0;
-    float times = 100000;
-    for (int i = 26; --i;) { Deck.push_back(1); }
-    for (int i = 5; --i;) { Deck.push_back(6); }
+    const auto start = std::chrono::high_resolution_clock::now();
+    std::vector<uint_fast8_t>Deck;    
+    uint_fast32_t ap=0;
+
+    srand(time(NULL));
+
+ 
+    for (int i = 26; --i;)
+        Deck.push_back(1);
+
+    for (int i = 5; --i;)  
+        Deck.push_back(6);
+
     Deck.shrink_to_fit();
-    for (; --times;)
+    uint_fast32_t __t = _t;
+
+    for (; --__t;)
     {
         std::vector<int>Hand;
         
-        for (int i = 6; i--;)
+        for (int i = 7; --i;)
         {
-            int k = Deck[rand() % 28];
+            const uint_fast8_t k = Deck[rand() % 28];
             Hand.push_back(k);            
             if (k == 6)
                 ++ap;
         }              
     }
-    float percent = ((ap / 100000) * 100);
+    const double percent = ap / _t;
     std::cout << percent << "%\n";
     std::cout << ap << std::endl;
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    std::cout << duration.count();
+    const auto stop = std::chrono::high_resolution_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << duration.count() << " MilliSeconds\n";
     system("PAUSE>0");
 }
