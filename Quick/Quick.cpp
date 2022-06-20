18ecd378c5d21eaea2b001fab00f5a8dbc8731f7
TiltedDFA <Madtac0alliance@gmail.com>
2022-06-20 01:27:06 +0100

    first mulithreading

--- Quick/Quick.cpp
+++ Quick/Quick.cpp
@@ -6,32 +6,52 @@
 #include <chrono>
 #include <random>
 #include <list>
+#include <thread>
+#include <future>
 
-constexpr int _t = 1000000000;
+constexpr int _t = 10000000;
 
-int main()
-{     
-    const auto start = std::chrono::high_resolution_clock::now();
-    std::array<int_fast8_t,30>Deck = {
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,6,6,6,6
-    };
+int fun_1() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> rng_coin(0, 29);
-    int ap=0;
-    int_fast32_t __t = _t;
+    std::vector<int_fast8_t>deck = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6
+    };
+    int ap = 0;
+    int __t = (_t / 9);
     for (; --__t;)
     {
         for (int i = 7; --i;)
         {
-            const int_fast8_t k = Deck[rng_coin(gen)];                       
-            if (k == 6)
+            const int_fast8_t k = rng_coin(gen);
+            if (deck[k] == 6)
+            {
                 ++ap;
-        }              
+                deck[k] = 1;
+            }
+        }
+        deck = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6
+        };
     }
-    auto percent = (ap / _t);
-    std::cout << percent << "%\n";
-    std::cout << ap << std::endl;
+    return ap;
+}
+int main()
+{     
+    const auto start = std::chrono::high_resolution_clock::now();
+   
+    auto f1 = std::async(fun_1);
+    auto f2 = std::async(fun_1);
+    auto f3 = std::async(fun_1);
+    auto f4 = std::async(fun_1);
+    auto f5 = std::async(fun_1);
+    auto f6 = std::async(fun_1);
+    auto f7 = std::async(fun_1);
+    auto f8 = std::async(fun_1);
+    auto f9 = std::async(fun_1);
+    std::cout << (f1.get() + f2.get() + f3.get() + f4.get() + f5.get() + f6.get() + f7.get() + f8.get() + f9.get()) << std::endl;
+
     const auto stop = std::chrono::high_resolution_clock::now();
     const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
     std::cout << duration.count() << " MilliSeconds\n";
